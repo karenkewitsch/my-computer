8ab79de4ac1768c4a1419651084389574f63f564
Markus Karenkewitsch <markusks91@me.com>
2023-07-26 11:38:37 +0200

    added boilerplate zshrc

---
+++ boilerplate.zshrc
@@ -0,0 +1,19 @@
+
+# ZPLUG
+source ~/.zplug/init.zsh
+zplug "zsh-users/zsh-completions"
+zplug "zsh-users/zsh-autosuggestions"
+zplug "zsh-users/zsh-syntax-highlighting", defer:2
+zplug "zsh-users/zsh-history-substring-search", defer:3
+zplug load
+
+# ALIASES
+alias activate_venv=". .venv/bin/activate"
+alias create_venv="python -m venv .venv"
+
+# PYENV
+export PYENV_ROOT="$HOME/.pyenv"
+command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
+
+# ENV VARS
--- init_new_mac.zsh
+++ init_new_mac.zsh
@@ -5,9 +5,6 @@ this_dir=${0:a:h}
 # installing homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-# installing Oh-my-zsh before anything else as it will create a .zshrc
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
 # see hidden files in Finder
 defaults write com.apple.Finder AppleShowAllFiles true
 killall Finder
@@ -19,6 +16,5 @@ brew install pyenv
 echo "#custom zsh commands" >> ~/.zshrc
 echo "path=('$this_dir/zsh_commands' \$path)" >> ~/.zshrc
 
-#defining oh-my-zsh plugins
-sed -i '.bak' 's/plugins=.*/plugins=(git gitfast)/g' ~.zshrc
-rm ~.zshrc.bak
+# zplug
+git clone https://github.com/zplug/zplug ~/.zplug
