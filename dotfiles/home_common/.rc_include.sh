90d832e324f877fe46e07babd2f8f4a8e36adf9a
Markus Sørensen <mso@lindcapital.com>
2025-09-06 16:30:56 +0200

    Fixed to look for funcs in home dir

--- dotfiles/home_common/.rc_include.sh
+++ dotfiles/home_common/.rc_include.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 # Load custom scripts
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-# Loop through each file in the 'funcs' directory and source it
-for file in "$SCRIPT_DIR/.bash_funcs/"*; do
+for file in "$HOME/.bash_funcs/"*; do
     if [ -f "$file" ]; then
         source "$file"
     fi
