879e91a354fbce61a7337efada48af54f0c3b617
Markus Karenkewitsch <mso@lindcapital.com>
2025-12-11 19:38:45 +0100

    Edits due to new macbook

--- installs_mac.zsh
+++ installs_mac.zsh
@@ -2,21 +2,28 @@
 
 this_dir=${0:a:h}
 
-# see hidden files in Finder
-defaults write com.apple.Finder AppleShowAllFiles true
-killall Finder
-
 # installing homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+eval "$(/opt/homebrew/bin/brew shellenv)"
 # some brew installs
 brew install --cask lunar # https://lunar.fyi/
 brew install stow
 
+brew install fontconfig
 brew install font-hack-nerd-font
+brew install --cask font-caskaydia-mono-nerd-font
+
 brew install starship # shell-prompt
 brew install fzf # fuzzy finder -> github.com/junegunn/fzf
 brew install alacritty
 brew install eza
+brew install --cask visual-studio-code
 
 # rust install
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+# set settings config
+defaults write com.apple.Finder AppleShowAllFiles true
+killall Finder
+defaults write com.apple.dock autohide 1
+defaults write "Apple Global Domain" _HTHideMenuBar 1
