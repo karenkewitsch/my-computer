06a59f6db5b6de69e7938458d5e442d01a235f63
Markus Karenkewitsch <markusks91@me.com>
2026-04-16 08:02:43 +0200

    added macos export of ssl cert path

--- dotfiles/home_mac/.zshrc_include.zsh
+++ dotfiles/home_mac/.zshrc_include.zsh
@@ -3,6 +3,7 @@ eval "$(/opt/homebrew/bin/brew shellenv)"
 
 # ENV VARS
 export PATH="$HOME/.local/bin:$PATH"
+export SSL_CERT_FILE=/etc/ssl/cert.pem
 
 # Load common shell stuff
 source ~/.zshrc_common.zsh
